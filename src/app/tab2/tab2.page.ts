0ebad6ad4effb31c314ad8e73d9b221cfa4d1485
goodtoseeu57 <46875261+goodtoseeu57@users.noreply.github.com>
2019-11-17 03:28:57 +0200

    reactive forms work

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,25 +2,50 @@ import {Component, OnInit} from '@angular/core'
 import {Employee} from "../model";
 import {ApiService} from "../api.service";
 import {Observable} from "rxjs";
+import {ReactiveFormsModule, FormBuilder, FormControl, FormGroup, Validators , NgForm , ValidatorFn , Validator} from "@angular/forms";
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page implements OnInit{
-  dataSourse = new Array<Employee>() ;
+export class Tab2Page implements OnInit {
+  dataSourse = new Array<Employee>();
 
-  constructor(private apiService: ApiService) {}
+  loginForm = new FormGroup({
+    email: new FormControl('' , Validators.compose([])),
+    password: new FormControl('' , Validators.compose([]))
+  });
+
+  constructor(private apiService: ApiService, private formbuilder: FormBuilder) {
+    this.loginForm = this.formbuilder.group({
+      email: [''],
+      password: ['']
+    });
+  }
 
   value = 1;
+
   ngOnInit(): void {
     this.getRestApi();
 
+
+
   }
 
+  formFuntion() {
+
+  }
+
+  onLogin() {
+
+      console.log('everything is alright');
+
+  }
+
+
   getRestApi(): void {
-    this.apiService.getEmployess().subscribe((response: Employee[]) => this.dataSourse = response );
+    this.apiService.getEmployess().subscribe((response: Employee[]) => this.dataSourse = response);
   }
 
 }
